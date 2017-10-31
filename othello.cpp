28e2d354d4d06bf65f111cfa9b69c88e547803e7
Mitchell Openlander <mopenla@bgsu.edu>
2017-10-31 11:21:40 -0400

    skeleton

---
+++ othello.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * Othello Game with Alpha-Beta Pruning
+ *
+ * Developed by: Mitchell Openlander
+ * For: CS 4200 - Artificial Intelligence Methods
+ *      Doctor Jake J.K. Lee
+ *
+ * //TODO general method used and whatnot
+ ******************************************************************************/
+
+#include <string>
+
+using namespace std;
+
+//Function Prototypes
+ 
+int main() {
+    return 0;
+} 
